75c463e215007dcc9432a435d398769903888b0f
rodrigonahid <rodrigonahid1@gmail.com>
2022-05-03 21:46:49 -0300

    creating modal toggle

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,25 +1,22 @@
-import { useState } from "react";
+import { Popover } from "@headlessui/react";
 import { ChatTeardropDots } from "phosphor-react";
+import { WidgetForm } from "./WidgetForm";
 
 export function Widget() {
-  const [isOpened, setIsOpened] = useState(false);
-
-  const toggleOpen = () => setIsOpened(!isOpened);
-
   return (
     <>
-      {isOpened && "is Open!"}
-      <div className="absolute right-4 bottom-4">
-        <button
-          onClick={toggleOpen}
-          className="bg-brand-500 hover:bg-brand-400 rounded-full px-3 h-12 text-white flex items-center group"
-        >
+      <Popover className="absolute right-5 bottom-5 flex flex-col items-end">
+        <Popover.Panel>
+          <WidgetForm />
+        </Popover.Panel>
+
+        <Popover.Button className="bg-brand-500 hover:bg-brand-400 rounded-full px-3 h-12 text-white flex items-center group">
           <ChatTeardropDots className="w-6 h-6" />
           <div className="max-w-0 group-hover:max-w-xs transition-all overflow-hidden">
             <span className="ml-3">Feedback</span>
           </div>
-        </button>
-      </div>
+        </Popover.Button>
+      </Popover>
     </>
   );
 }
